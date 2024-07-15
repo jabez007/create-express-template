07abab462ed1a00b474ede02cff7622c747d6865
Jimmy <jabez007@users.noreply.github.com>
2024-07-15 12:35:13 -0500

    log both request and response

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const requestId = require('~utils/request-id')
-const morgan = require('~utils/morgan.js')
+const { requestMorgan, responseMorgan } = require('~utils/morgan.js')
 
 const app = express()
 
 app.use(requestId())
 
-app.use(morgan)
+app.use(requestMorgan)
+app.use(responseMorgan)
 
 app.get('/', (req, res) => {
   res.send('Hello World')
--- src/utils/morgan.js
+++ src/utils/morgan.js
@@ -4,12 +4,26 @@ morgan.token('reqid', function (req, res) {
   return req.id
 })
 
-module.exports = morgan(
-  // Define message format string (this is the default one).
-  // The message format is made from tokens, and each token is
-  // defined inside the Morgan library.
-  // You can create your custom token to show what do you want from a request.
-  ':method :url :reqid :status :res[content-length] - :response-time ms'
-  // Options:
-  // { stream, skip }
-)
+module.exports = {
+  requestMorgan: morgan(
+    // Define message format string.
+    // The message format is made from tokens, and each token is
+    // defined inside the Morgan library.
+    // You can create your custom token to show what do you want from a request.
+    ':reqid :method :url',
+    // Options:
+    // { stream, skip }
+    {
+      immediate: true
+    }
+  ),
+  responseMorgan: morgan(
+    // Define message format string (this is the default one).
+    // The message format is made from tokens, and each token is
+    // defined inside the Morgan library.
+    // You can create your custom token to show what do you want from a request.
+    ':reqid :status :res[content-length] - :response-time ms'
+    // Options:
+    // { stream, skip }
+  )
+}
