89331cc1ad37c4f495bf728d8897aeda10fc270a
Jimmy <jabez007@users.noreply.github.com>
2024-08-26 16:14:07 -0500

    inject logger into request context

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const requestId = require('~utils/request-id')
-const { requestMorgan, responseMorgan } = require('~utils/morgan.js')
+const loggerMiddleware = require('~utils/loggerMiddleware')
+const { requestMorgan, responseMorgan } = require('~utils/morgan')
 
 const app = express()
 
@@ -9,7 +10,14 @@ app.use(requestId())
 app.use(requestMorgan)
 app.use(responseMorgan)
 
+app.use(loggerMiddleware)
+
 app.get('/', (req, res) => {
+  req.logger.error('This is an error log')
+  req.logger.warn('This is a warn log')
+  req.logger.info('This is a info log')
+  req.logger.debug('This is a debug log')
+
   res.send('Hello World')
 })
 
