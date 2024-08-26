0933c8021f65dc26c877a68973939c3f86429a29
Jimmy <jabez007@users.noreply.github.com>
2024-08-26 16:14:11 -0500

    gracefully shutdown server

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const app = require('./server.js')
 
 const port = process.env.PORT || 8888
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`)
+})
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received, closing HTTP server')
+  server.close(() => {
+    console.log('HTTP server closed')
+  })
 })
