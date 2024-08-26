bab1240db4a6f3b004da405c9c83f704d697b800
Jimmy <jabez007@users.noreply.github.com>
2024-08-26 16:14:29 -0500

    add JSdoc to request-id middleware

--- src/utils/request-id.js
+++ src/utils/request-id.js
@@ -6,7 +6,15 @@ function generateV4UUID (_request) {
 
 const ATTRIBUTE_NAME = 'id'
 
-module.exports = function requestID ({
+/**
+ * Create an Express middleware function to inject an "id" in to each incoming request
+ * @param {Object} param0
+ * @param {function(ExpressRequest): string} param0.generator - A function to generate the unique Id for the incoming request
+ * @param {string} param0.headerName - Which header to look for in the incoming request for a unique Id
+ * @param {boolean} param0.setHeader - Should the outgoing response include the header for the unique Id?
+ * @returns {function(ExpressRequest, ExpressResponse, ExpressNextFunction): void} - The middleware function to be passed to Express
+ */
+module.exports = function requestId ({
   generator = generateV4UUID,
   headerName = 'X-Request-Id',
   setHeader = true
